1d53292f236d75040746c8415c1db214a4ef87e6
AI-MOO <eemoh.osman@gmail.com>
2022-05-30 22:02:42 +0300

    size mappings

---
+++ 03-Visualization/03_size_mappings_ggplot.R
@@ -0,0 +1,10 @@
+# importing libraries
+library(tidyverse)
+library(ggplot2)
+
+# importing dataaAA
+mpg = ggplot2::mpg
+
+# scatter plot visulaiztion + aesthetic mappings
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, size = class))
