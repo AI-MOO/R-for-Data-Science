6ef7ce6ef82363045dc1bd26125ef79c3c8dd180
AI-MOO <eemoh.osman@gmail.com>
2022-05-30 22:04:20 +0300

    size mapping with continious variable

---
+++ 03-Visualization/07_size_mappings_with_cont_data.R
@@ -0,0 +1,12 @@
+# importing libraries
+library(tidyverse)
+library(ggplot2)
+
+# importing data
+mpg = ggplot2::mpg
+A
+# right
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, size=cty))
+
+
