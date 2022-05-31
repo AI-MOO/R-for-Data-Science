126f32d5b00f23c89e4987462c180fa58c02a5ea
AI-MOO <eemoh.osman@gmail.com>
2022-05-31 19:58:20 +0300

    geom_smooth line plots with two variables

---
+++ 03-Visualization/16_geom_smooth_two_vars.R
@@ -0,0 +1,19 @@
+# importing libraries
+library(tidyverse)
+library(ggplot2)
+
+# importing data
+mpg = ggplot2::mpg
+
+# multiple plots (3 plots)
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x = displ, y = hwy, group = drv))
+
+ggplot(data = mpg) +
+  geom_smooth(
+    mapping = aes(x = displ, y = hwy, color = drv),
+    show.legend = FALSE
+  )
