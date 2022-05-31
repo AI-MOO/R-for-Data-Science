0be6644f65f9950d4e91217630b809c90abe4bfc
AI-MOO <eemoh.osman@gmail.com>
2022-05-31 20:35:40 +0300

    geom_bar, coord_flip, coord_polar, labs: labels

---
+++ 03-Visualization/43_bar_and_coxcomb.R
@@ -0,0 +1,15 @@
+# importing libraries
+library(tidyverse)
+library(ggplot2)
+
+bar <- ggplot(data = diamonds) + 
+  geom_bar(
+    mapping = aes(x = cut, fill = cut), 
+    show.legend = FALSE,
+    width = 1
+  ) + 
+  theme(aspect.ratio = 1) +
+  labs(x = NULL, y = NULL)
+
+bar + coord_flip()
+bar + coord_polar()
