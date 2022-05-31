d6db89e4883bafc9c154721bb50178b1d5d0c27c
AI-MOO <eemoh.osman@gmail.com>
2022-05-31 20:41:25 +0300

    variables assignment <-

--- 04_Workflow_basics/02_variables_assignment.R
+++ 04_Workflow_basics/02_variables_assignment.R
@@ -1,9 +1,3 @@
-# variables styling
-
-i_use_snake_case
-otherPeopleUseCamelCase
-some.people.use.periods
-And_aFew.People_RENOUNCEconvention
-
-# variable can be a combination of letters, nums and underscores
-this_is_a_really_long_name <- 2.5
+x <- 3 * 4
+y <- "Hello World!"
+z <- 12.9
