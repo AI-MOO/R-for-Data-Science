dc12202a36332cc2afa6d6b90c04ba9b233428ed
AI-MOO <eemoh.osman@gmail.com>
2022-05-31 20:20:55 +0300

    geom_boxplot, coord flip (flip x & y axis)

--- 03-Visualization/41_coord_flip.R
+++ 03-Visualization/41_coord_flip.R
@@ -2,11 +2,8 @@
 library(tidyverse)
 library(ggplot2)
 
-nz <- map_data("nz")
-
-ggplot(nz, aes(long, lat, group = group)) +
-  geom_polygon(fill = "white", colour = "black")
-
-ggplot(nz, aes(long, lat, group = group)) +
-  geom_polygon(fill = "white", colour = "black") +
-  coord_quickmap()
+ggplot(data = mpg, mapping = aes(x = class, y = hwy)) + 
+  geom_boxplot()
+ggplot(data = mpg, mapping = aes(x = class, y = hwy)) + 
+  geom_boxplot() +
+  coord_flip()
