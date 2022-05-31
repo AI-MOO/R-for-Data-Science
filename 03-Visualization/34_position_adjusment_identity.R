e9815f64ea3a48c415feef96c4a191e5b7f36416
AI-MOO <eemoh.osman@gmail.com>
2022-05-31 20:15:09 +0300

    position argument as "identity"

---
+++ 03-Visualization/34_position_adjusment_identity.R
@@ -0,0 +1,9 @@
+# importing libraries
+library(tidyverse)
+library(ggplot2)
+
+ggplot(data = diamonds, mapping = aes(x = cut, fill = clarity)) + 
+  geom_bar(alpha = 1/5, position = "identity")
+
+ggplot(data = diamonds, mapping = aes(x = cut, colour = clarity)) + 
+  geom_bar(fill = NA, position = "identity")
