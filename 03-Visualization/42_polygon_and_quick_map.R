c12a5e1de1eac1249676510826653e02c4b56cee
AI-MOO <eemoh.osman@gmail.com>
2022-05-31 20:21:12 +0300

    geom_polygon, coord_quickmap

---
+++ 03-Visualization/42_polygon_and_quick_map.R
@@ -0,0 +1,8 @@
+nz <- map_data("nz")
+
+ggplot(nz, aes(long, lat, group = group)) +
+  geom_polygon(fill = "white", colour = "black")
+
+ggplot(nz, aes(long, lat, group = group)) +
+  geom_polygon(fill = "white", colour = "black") +
+  coord_quickmap()
