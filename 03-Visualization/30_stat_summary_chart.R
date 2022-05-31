02373f354362e654a087906b23062e23769a0e53
AI-MOO <eemoh.osman@gmail.com>
2022-05-31 20:09:02 +0300

    stat_summary, fun.min = min,     fun.max = max,     fun = median

---
+++ 03-Visualization/30_stat_summary_chart.R
@@ -0,0 +1,12 @@
+# importing libraries
+library(tidyverse)
+library(ggplot2)
+
+# statistical summary chart
+ggplot(data = diamonds) + 
+  stat_summary(
+    mapping = aes(x = cut, y = depth),
+    fun.min = min,
+    fun.max = max,
+    fun = median
+  )
