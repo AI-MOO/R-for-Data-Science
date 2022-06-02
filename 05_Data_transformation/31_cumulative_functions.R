44d13d3875e8dd7c2a4fe2f4f7a8fc9fedb7cadd
AI-MOO <eemoh.osman@gmail.com>
2022-06-02 21:46:01 +0300

    cumulative functions

---
+++ 05_Data_transformation/31_cumulative_functions.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(nycflights13)
+
+# Cumulative and rolling aggregates: R provides functions 
+# for running sums,products, mins and maxes: 
+# cumsum(), cumprod(), cummin(), cummax();
+
+
+(x <- 1:10)
+cumsum(x)
+cummean(x)
+
+
+
