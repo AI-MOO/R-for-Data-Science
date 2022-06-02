5f9d2466de0c582a2749c0c26b612f4182479dc6
AI-MOO <eemoh.osman@gmail.com>
2022-06-02 21:47:33 +0300

    ranking techniques

---
+++ 05_Data_transformation/33_rank_functions.R
@@ -0,0 +1,13 @@
+library(nycflights13)
+library(tidyverse)
+
+y <- c(1, 2, 2, NA, 3, 4)
+
+row_number(y)
+#> [1]  1  2  3 NA  4  5
+dense_rank(y)
+#> [1]  1  2  2 NA  3  4
+percent_rank(y)
+#> [1] 0.00 0.25 0.25   NA 0.75 1.00
+cume_dist(y)
+#> [1] 0.2 0.6 0.6  NA 0.8 1.0
