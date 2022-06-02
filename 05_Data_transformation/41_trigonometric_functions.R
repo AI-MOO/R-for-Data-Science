1d632d37995d8247de0c81bd12828295681a9364
AI-MOO <eemoh.osman@gmail.com>
2022-06-02 21:48:31 +0300

    trigonometric functions

---
+++ 05_Data_transformation/41_trigonometric_functions.R
@@ -0,0 +1,28 @@
+# defining a sequence of numbers
+x <- seq(-3, 7, by = 1 / 2)
+
+
+# trigonometric functions in degrees
+sin(pi * x)
+cos(pi * x)
+tan(pi * x)
+
+# pi vlaue
+pi
+pi <- "Apple"
+base::pi
+
+# trigonometric functions in radinas
+sinpi(x)
+cospi(x)
+tanpi(x)
+
+# arc trigonometric functions
+x <- seq(-1, 1, by = 1 / 4)
+acos(x)
+asin(x)
+atan(x)
+atan2(c(1, 0, -1, 0), c(0, 1, 0, -1))
+
+
+
