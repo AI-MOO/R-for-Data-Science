392c06c7673c7a61af8618b7b5a7a941f0c11b9b
AI-MOO <eemoh.osman@gmail.com>
2022-06-02 21:48:21 +0300

    adding two vectors

---
+++ 05_Data_transformation/40_adding_two_vectors.R
@@ -0,0 +1,9 @@
+# adding two vectors 
+1:3 + 1:10
+#> Warning in 1:3 + 1:10: longer object length is not a multiple of shorter object
+#> length
+#>  [1]  2  4  6  5  7  9  8 10 12 11
+
+
+# This is equivalent to the following.
+c(1 + 1, 2 + 2, 3 + 3, 1 + 4, 2 + 5, 3 + 6, 1 + 7, 2 + 8, 3 + 9, 1 + 10)
